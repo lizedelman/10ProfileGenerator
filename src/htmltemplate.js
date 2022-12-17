248f81c0db3138889686c6448aa78ac476189a3a
Wild Carrot <edelman.eliz@gmail.com>
2022-12-17 13:18:39 -0700

    Changed colors in bootstrap html page

--- src/htmltemplate.js
+++ src/htmltemplate.js
@@ -2,7 +2,7 @@ const generateTeam = (team) => {
   const generateManager = (manager) => {
     return `
 <div class="card employee-card">
-    <div class="card-header bg-primary text-white">
+    <div class="card-header bg-info text-white">
         <h2 class="card-title">${manager.getName()}</h2>
         <h3 class="card-title"><i class="fas fa-mug-hot mr-2"></i>${manager.getRole()}</h3>
     </div>
@@ -17,11 +17,11 @@ const generateTeam = (team) => {
         `;
   };
   const generateEngineer = (engineer) => {
-    return `<div class="card employee-card"> <div class="card-header bg-primary text-white"> <h2 class="card-title">${engineer.getName()}</h2> <h3 class="card-title"><i class="fas fa-glasses mr-2"></i>${engineer.getRole()}</h3> </div> <div class="card-body"> <ul class="list-group"> <li class="list-group-item">ID: ${engineer.getId()}</li> <li class="list-group-item">Email: <a href="mailto:${engineer.getEmail()}">${engineer.getEmail()}</a></li> <li class="list-group-item">GitHub: <a href="https://github.com/${engineer.getGithub()}" target="_blank" rel="noopener noreferrer">${engineer.getGithub()}</a></li> </ul> </div></div> `;
+    return `<div class="card employee-card"> <div class="card-header bg-info text-white"> <h2 class="card-title">${engineer.getName()}</h2> <h3 class="card-title"><i class="fas fa-glasses mr-2"></i>${engineer.getRole()}</h3> </div> <div class="card-body"> <ul class="list-group"> <li class="list-group-item">ID: ${engineer.getId()}</li> <li class="list-group-item">Email: <a href="mailto:${engineer.getEmail()}">${engineer.getEmail()}</a></li> <li class="list-group-item">GitHub: <a href="https://github.com/${engineer.getGithub()}" target="_blank" rel="noopener noreferrer">${engineer.getGithub()}</a></li> </ul> </div></div> `;
   };
 
   const generateIntern = (intern) => {
-    return `<div class="card employee-card"> <div class="card-header bg-primary text-white"> <h2 class="card-title">${intern.getName()}</h2> <h3 class="card-title"><i class="fas fa-user-graduate mr-2"></i>${intern.getRole()}</h3> </div> <div class="card-body"> <ul class="list-group"> <li class="list-group-item">ID: ${intern.getId()}</li> <li class="list-group-item">Email: <a href="mailto:${intern.getEmail()}">${intern.getEmail()}</a></li> <li class="list-group-item">School: ${intern.getSchool()}</li> </ul> </div></div> `;
+    return `<div class="card employee-card"> <div class="card-header bg-info text-white"> <h2 class="card-title">${intern.getName()}</h2> <h3 class="card-title"><i class="fas fa-user-graduate mr-2"></i>${intern.getRole()}</h3> </div> <div class="card-body"> <ul class="list-group"> <li class="list-group-item">ID: ${intern.getId()}</li> <li class="list-group-item">Email: <a href="mailto:${intern.getEmail()}">${intern.getEmail()}</a></li> <li class="list-group-item">School: ${intern.getSchool()}</li> </ul> </div></div> `;
   };
 
   const html = [];
@@ -55,7 +55,7 @@ module.exports = (team) => {
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-    <title>My Team</title>
+    <title>ACME Team</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
         integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" href="style.css">
@@ -64,8 +64,8 @@ module.exports = (team) => {
 <body>
     <div class="container-fluid">
         <div class="row">
-            <div class="col-12 jumbotron mb-3 team-heading bg-danger">
-                <h1 class="text-center text-white">My Team</h1>
+            <div class="col-12 jumbotron mb-3 team-heading bg-dark">
+                <h1 class="text-center text-white">ACME Group</h1>
             </div>
         </div>
     </div>
